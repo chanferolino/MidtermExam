fec3708183f798d5555af08dd531f32aa892b8e6
chanferolino <chanferolino@gmail.com>
2016-02-02 19:45:52 +0800

    Added menu clear functionality

--- app/src/main/java/sidespell/tech/midtermexam/MainActivity.java
+++ app/src/main/java/sidespell/tech/midtermexam/MainActivity.java
@@ -1,17 +1,42 @@
 package sidespell.tech.midtermexam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import sidespell.tech.midtermexam.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainFragment mListViewFragment;
+    private EditText etAlbum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etAlbum = (EditText) findViewById(R.id.etAlbum);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear) {
+            etAlbum.setText("");
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
